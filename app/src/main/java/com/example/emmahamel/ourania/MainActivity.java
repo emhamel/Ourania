21f47ac08da83c0ee0c43e0d46206446a3bcd9d1
emhamel <ehamel@keyschool.org>
2018-11-27 18:24:14 -0600

    hi amit

--- app/src/main/java/com/example/emmahamel/ourania/MainActivity.java
+++ app/src/main/java/com/example/emmahamel/ourania/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.emmahamel.ourania;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.content.Intent;
 import android.provider.MediaStore;
 
@@ -19,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
